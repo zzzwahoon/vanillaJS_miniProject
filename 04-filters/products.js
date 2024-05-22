af90f285304468e0d6d5e74ba2e35bdd45b66d87
ZzzwaHoon <kkjw94@gmail.com>
2024-05-22 14:13:11 +0900

    04-filters-SETUP

---
+++ 04-filters/products.js
@@ -0,0 +1,114 @@
+const products = [
+  {
+    "createdAt": "2023-12-18T00:45:34.612Z",
+    "title": "Hamburger",
+    "price": "877.00",
+    "company": "Burger King",
+    "colors": [
+      "#f15025",
+      "#222"
+    ],
+    "image": "https://res.cloudinary.com/jaewon/image/upload/v1702904734/foods/v9zr894q4mwlostkcqw4.jpg",
+    "description": "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
+    "id": "1",
+    "featured": false
+  },
+  {
+    "createdAt": "2023-12-17T15:35:41.422Z",
+    "title": "Hat",
+    "price": "114.00",
+    "company": "Pancake Company",
+    "colors": [
+      "#f15025",
+      "#222"
+    ],
+    "image": "https://res.cloudinary.com/jaewon/image/upload/v1702904733/foods/elv7pex4gxdr35povdwi.jpg",
+    "description": "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
+    "id": "2",
+    "featured": true
+  },
+  {
+    "createdAt": "2023-12-17T17:54:09.638Z",
+    "title": "BreakFast",
+    "price": "884.00",
+    "company": "Breakfast Company",
+    "colors": [
+      "#f15025",
+      "#222"
+    ],
+    "image": "https://res.cloudinary.com/jaewon/image/upload/v1702904733/foods/uhufmkdv31q27oqe3zcq.jpg",
+    "description": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
+    "id": "3",
+    "featured": true
+  },
+  {
+    "createdAt": "2023-12-18T07:08:34.207Z",
+    "title": "Meat",
+    "price": "996.00",
+    "company": "Meat Company",
+    "colors": [
+      "#f15025",
+      "#222"
+    ],
+    "image": "https://res.cloudinary.com/jaewon/image/upload/v1702904731/foods/azfiuardcftjywuuhwnk.jpg",
+    "description": "The Football Is Good For Training And Recreational Purposes",
+    "id": "4",
+    "featured": true
+  },
+  {
+    "createdAt": "2023-12-17T15:08:09.478Z",
+    "title": "Egg",
+    "price": "520.00",
+    "company": "Egg Company",
+    "colors": [
+      "#f15025",
+      "#222"
+    ],
+    "image": "https://res.cloudinary.com/jaewon/image/upload/v1702904729/foods/qthxioei0vai3qcz2rr1.jpg",
+    "description": "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+    "id": "5",
+    "featured": false
+  },
+  {
+    "createdAt": "2023-12-17T15:26:35.659Z",
+    "title": "Milk Shake",
+    "price": "819.00",
+    "company": "Egg Company",
+    "colors": [
+      "#f15025",
+      "#222"
+    ],
+    "image": "https://res.cloudinary.com/jaewon/image/upload/v1702904726/foods/rgvywqtv56h6uwzucbfr.jpg",
+    "description": "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+    "id": "6",
+    "featured": false
+  },
+  {
+    "createdAt": "2023-12-18T02:03:15.658Z",
+    "title": "Cheese Burger",
+    "price": "232.00",
+    "company": "Burger King",
+    "colors": [
+      "#f15025",
+      "#222"
+    ],
+    "image": "https://res.cloudinary.com/jaewon/image/upload/v1702904727/foods/akqrqa937kdzqjzlfudv.jpg",
+    "description": "The Football Is Good For Training And Recreational Purposes",
+    "id": "7",
+    "featured": false
+  },
+  {
+    "createdAt": "2023-12-18T07:04:06.400Z",
+    "title": "Bread",
+    "price": "108.00",
+    "company": "Breakfast Company",
+    "colors": [
+      "#f15025",
+      "#222"
+    ],
+    "image": "https://res.cloudinary.com/jaewon/image/upload/v1702904730/foods/qq6fijkhv9aztbpytjan.jpg",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+    "id": "8",
+    "featured": false
+  }
+]
